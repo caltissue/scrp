46f8b5d073b22f0e12b2da705eb4cbf9b231f23a
cal <caltissue@gmail.com>
2021-06-09 17:31:21 -0700

    more loc reduction & organization

--- analytics.py
+++ analytics.py
@@ -2,79 +2,54 @@ import os
 import db_func as db
 import string
 
-def my_common_words():
-    return [
-    'but', 'and', 'so', 'if', 'because', 'a', '', ' ', '-', '--', 'has', 'one',
-    'well', 'then',
-    ]
-
-def junk_char_list():
-    return [
-    '/', '(', ')', ',', '. ', '.\n', '.\r', '.\t', '..', '!', '?', '|', ':', '*',
-    '=', '•', '●', '"', '\n', '\r', '\t', '_', '“', '”', ';', '·', '📺', '👉',
-    '‘', '`', '[', ']', '{', '}', ' +', '►', '✭',
-    ]
-
-def ok_chars():
-    return string.ascii_lowercase + string.digits + string.whitespace
+ok_chars = string.ascii_lowercase + string.digits + string.whitespace
 
 def get_common_words():
-    query = '''
-    SELECT word
-    FROM wordcount_body
-    WHERE count > (
-        SELECT COUNT(id) / 2
-        FROM craigslist_jobs
-    )
-    '''
-    commonwords = []
-    commonword_results = db.execute_query(query)
-    for c in commonword_results:
-        commonwords.append(c[0])
+    commonword_res = db.execute_query('''
+    SELECT word FROM wordcount_body
+    WHERE count > (SELECT COUNT(id) / 2 FROM craigslist_jobs)
+    ''')
+    commonwords = [c[0] for c in commonword_res]
 
-    for w in my_common_words():
-        commonwords.append(w)
-
+    commonwords.extend([
+    'but', 'and', 'so', 'if', 'because', 'a', '', ' ', '-', '--', 'has', 'one',
+    'well', 'then',
+    ])
     return commonwords
 
 def word_pairs():
     wordpair_counts = {}
     post_bodies = db.execute_query('SELECT body FROM craigslist_jobs')
     commons = get_common_words()
-    okchars = ok_chars()
     for b in post_bodies:
         body = b[0].lower()
-
-        body = ''.join([c for c in body if c in okchars])
+        body = ''.join([c for c in body if c in ok_chars])
 
         words = set([word for word in body.split() if word not in commons])
 
         pairs = set([tuple(sorted((i,j))) for i in words for j in words if i != j])
-
         for tup in pairs:
             if tup in wordpair_counts: wordpair_counts[tup] += 1
             else: wordpair_counts[tup] = 1
 
     return wordpair_counts
 
-def get_high_occurence_pairs(word, mincount):
+def get_frequent_pairs(word, mincount):
     pairs = word_pairs()
-    mytups = [t for t in pairs.keys() if word in t]
-    mypairs = {}
-    for t in mytups: mypairs[t] = pairs[t]
-    my_highoccurence_tups = [t for t in mypairs.keys() if mypairs[t] >= mincount]
-    return my_highoccurence_tups
+    mytups = [t for t in pairs if word in t]
+    mypairs = {t: pairs[t] for t in mytups}
+    my_frequent_tups = [t for t in mypairs if mypairs[t] >= mincount]
+    return my_frequent_tups
 
 def wordcount(column):
     wordmap = {}
     wordcountlist = []
     commons = get_common_words()
-    okchars = ok_chars()
     titles = db.execute_query('SELECT %s FROM craigslist_jobs' % column)
 
     for t in titles:
         title = t[0].lower()
-        title = ''.join([c for c in title if c in okchars])
+        title = ''.join([c for c in title if c in ok_chars])
 
         wordlist = set([word for word in title.split() if word not in commons])
 
--- db_func.py
+++ db_func.py
@@ -2,6 +2,18 @@ from mysql.connector import connect, Error
 import os, traceback
 from difflib import SequenceMatcher
 
+def print_error_info(e, query):
+    print(e)
+    print('\nQUERY:', query[:350])
+    print('\nTRACEBACK:')
+    traceback.print_exc()
+    print('\nSTACK:')
+    traceback.print_stack()
+
+'''
+    EXECUTE_QUERY
+    1 command, autocommit on, returns optional list of tuples (or None)
+'''
 def execute_query(query):
     res = []
     try:
@@ -16,57 +28,12 @@ def execute_query(query):
                 res = cursor.fetchall()
                 connection.commit()
     except Error as e:
-        print(e)
-        print('\nQUERY:', query)
-        print('\nTRACEBACK:')
-        traceback.print_exc()
-        print('\nSTACK:')
-        traceback.print_stack()
+        print_error_info(e, query)
     return res
-
-'''
-    TODO: build complete list, pass to abitrary insert method (when exists)
-'''
-def insert_records_from_tuples(t_list, table):
-    query = "INSERT INTO %s (count, word) VALUES ( %s, '%s' )"
-    for t in t_list: execute_query(query % (table, str(t[0]), t[1]))
 
 def truncate_table(table):
     q = 'TRUNCATE TABLE %s' % table
     execute_query(q)
-
-'''
-    TODO: extract general arbitrary method to batch large queries from lists
-'''
-def insert_wordpair_counts(wordpairs):
-    querybase = '''
-    INSERT INTO wordpairs (word1, word2, count)
-    VALUES
-    '''
-    insert_list = []
-    for k in wordpairs:
-        insert_list.append("( '%s', '%s', %s )" % (k[0], k[1], wordpairs[k]))
-    try:
-        with connect(
-            host='localhost',
-            user='root',
-            password=os.getenv('DB_PASS'),
-            database='scrp'
-        ) as connection:
-            with connection.cursor() as cursor:
-                cursor.execute('SET autocommit=0;')
-                while insert_list:
-                    query = querybase + ' '
-                    insertcount = 100000
-                    for i in range(min(len(insert_list), insertcount)):
-                        query += insert_list[i] + ', '
-                    query = query[:-2] + ';'
-                    insert_list = insert_list[insertcount:]
-                    cursor.execute(query)
-                cursor.execute('COMMIT;')
-                connection.commit()
-    except Error as e:
-        print(e)
 
 def get_all_post_ids(including_reposts):
     query = 'SELECT post_id FROM craigslist_jobs'
@@ -109,3 +76,50 @@ def add_post(p): # dict post
     '1' # times_encountered (this is a new post in theory)
     )
     execute_query(q)
+
+'''
+    INSERT_LARGE_QUERY_FROM_LIST
+    builds paged query for large inserts, issues multiple commands, autocommit off
+    no return value
+'''
+def insert_large_query_from_list(querybase, value_list):
+    try:
+        with connect(
+            host='localhost',
+            user='root',
+            password=os.getenv('DB_PASS'),
+            database='scrp'
+        ) as connection:
+            with connection.cursor() as cursor:
+                cursor.execute('SET autocommit=0;')
+                while value_list:
+                    query = querybase + ' '
+                    insertcount = 100000
+                    for i in range(min(len(value_list), insertcount)):
+                        query += value_list[i] + ', '
+                    query = query[:-2] + ';'
+                    value_list = value_list[insertcount:]
+                    cursor.execute(query)
+                cursor.execute('COMMIT;')
+                connection.commit()
+    except Error as e:
+        sample_values = value_list[:10]
+        query = '(truncated) ' + querybase + ' '
+        for i in range(len(sample_values)):
+            q += sample_values[i]
+        print_error_info(e, query)
+
+def insert_records_from_tuples(t_list, table):
+    querybase = "INSERT INTO %s (count, word) VALUES" % table
+    values = []
+    for t in t_list:
+        values.append("( %s, '%s' )" % (str(t[0]), t[1]))
+    insert_large_query_from_list(querybase, values)
+
+
+def insert_wordpair_counts(wordpairs):
+    querybase = "INSERT INTO wordpairs (word1, word2, count) VALUES"
+    values = []
+    for k in wordpairs:
+        values.append("( '%s', '%s', %s )" % (k[0], k[1], wordpairs[k]))
+    insert_large_query_from_list(querybase, values)
